996512e0e6e7d31ad82d9982184aec54411d2e22
FlytingLepruchaun <122673727+FlytingLepruchaun@users.noreply.github.com>
2026-04-26 14:36:23 +0000

    feat: implement trie load and unload functionality
    Add:
    -Trie() for node initialization
    -insert() for word insertion
    -load() to build trie from dictionary file
    -unload() to recursively free memory.

---
+++ trie.c
@@ -0,0 +1,95 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "trie.h"
+
+struct node
+{
+    struct node *children[26];
+    bool isWordEnd;
+};
+
+node *Trie(void)
+{
+    node *nn = malloc(sizeof(node));
+
+    if (nn == NULL)
+    {
+        printf("malloc failed.\n");
+        return NULL;
+    }
+    
+    for (int i = 0; i < 26; i++)
+    {
+        nn->children[i] = NULL;
+    }
+    nn->isWordEnd = false;
+
+    return nn;
+}
+
+void insert(node *root, char *word)
+{
+    node *ptr = root;
+    for (int i = 0; word[i] != '\0'; i++)
+    {
+        if (ptr->children[word[i] - 'a'] == NULL) 
+        {
+            node *nn = Trie();
+            ptr->children[word[i] - 'a'] = nn;
+        }
+
+        ptr = ptr->children[word[i] - 'a'];
+    }
+    ptr->isWordEnd = true;
+}
+
+node *load()
+{
+    FILE *dict = fopen("dict1.txt", "r");
+
+    if (dict == NULL)
+    {
+        printf("could not open dictionary.\n");
+        return NULL;
+    }
+    
+    node *root = Trie();
+
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    
+
+    char word[45] = "";
+    while (fgets(word, 45, dict))
+    {
+        word[strcspn(word, "\n")] = '\0';
+        insert(root, word);
+    }
+
+    fclose(dict);
+
+    return root;
+}
+
+void unload (node *root)
+{
+
+    if (root == NULL)
+    {
+        return;
+    }
+    
+    for (int i = 0; i < 26; i++)
+    {
+        if (root->children[i] != NULL)
+        {
+            unload(root->children[i]);
+        }
+    }
+
+    free(root);
+}
---
+++ trie.h
@@ -0,0 +1,8 @@
+#ifndef TRIE
+    #define TRIE
+    typedef struct node node;
+    
+    node *load();
+    void unload (node *root);
+
+#endif
