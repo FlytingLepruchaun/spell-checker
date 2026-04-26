89dd2f69dc9961456fdaded1ec8278e87655b508
FlytingLepruchaun <122673727+FlytingLepruchaun@users.noreply.github.com>
2026-04-26 18:05:18 +0000

    feat: Implement standard mode for spell check
    Read words from file, clean input, and check against trie dictionary to
    identify and count misspelled words.

---
+++ spell-checker.c
@@ -0,0 +1,66 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include "trie.h"
+
+void strlwr(char *str);
+
+int main(int argc, char const *argv[])
+{
+    printf("Enter adress of text file: ");
+    char txtfile[256];
+    scanf("%s", txtfile);
+
+    FILE *text = fopen(txtfile, "r");
+    if (text == NULL)
+    {
+        printf("Could not open text file.\n");
+        return 1;
+    }
+
+    node *root = load();
+    if (root == NULL)
+    {
+        printf("Could not load dictionary.\n");
+        fclose(text);
+        return 2;
+    }
+    
+    unsigned int misspelt = 0;
+    char word[25] = "";
+    printf("MISSPLET WORDS: \n");
+    while(fscanf(text, "%s", word) == 1)
+    {
+        unsigned int len = strlen(word);
+        while(len > 0 && ispunct((unsigned char)word[len - 1]))
+        {
+            word[--len] = '\0';
+        }
+        if (word[0] == '\0')
+        {
+            continue;   
+        } 
+        strlwr(word);
+        
+        if (!search(root, word))
+        {
+            printf("%s\n", word);
+            ++misspelt;
+        }
+    }
+
+    printf("\nTOTAL MISSPELT WORDS: %u\n", misspelt);
+    
+    unload(root);
+    fclose(text);
+    return 0;
+}
+
+void strlwr(char *str)
+{
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        str[i] = tolower((unsigned char)str[i]);
+    }
+}
